c37802829c06e355dcc24938a1a55c643c6b9bcd
vnkl25 <vykhanhle9971@gmail.com>
2026-05-06 09:29:13 -0500

    add school email restriction

--- src/App.js
+++ src/App.js
@@ -8,95 +8,19 @@ import { collection, doc, setDoc, getDocs } from "firebase/firestore";
 import { storage } from "./firebase";
 import { ref, uploadBytes, getDownloadURL, listAll } from "firebase/storage";
 
+import Game from "./Game";
 
-function Square({ value, onSquareClick }) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
+provider.setCustomParameters({
+  hd: "lewisu.edu"
+});
 
-function Board({ xIsNext, squares, onPlay }) {
-  function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
-    onPlay(nextSquares);
-  }
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-  }
-
-  return (
-    <>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </>
-  );
-}
-
-function Game({ user }) {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];
-
+function Profile({ user }) {
   // Added state to display all users 
   const [allUsers, setAllUsers] = useState([]);
 
   // Added state for file upload
   const [file, setFile] = useState(null);
   const [files, setFiles] = useState([]);
-
-  function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
-  }
-
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
-  }
-
-  const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
-      description = 'Go to game start';
-    }
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
-  });
 
   // Added save current user info to Firestore 
   const saveUser = async () => {
@@ -179,19 +103,14 @@ useEffect(() => {
 }, [user.uid]);
 
   return (
-    <div className="game">
+    <div className="user">
       {/* Added logout */} 
       <div style={{ position: 'absolute', top: '10px', left: '20px' }}>
         <p>Welcome, {user.displayName}</p>
         <button onClick={() => signOut(auth)}>Logout</button>
       </div>
 
-      <div className="game-board" style={{ marginTop: '100px' }}>
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-      </div>
-
-      <div className="game-info" style={{ marginTop: '100px' }}>
-        <ol>{moves}</ol>
+      <div className="user-info" style={{ marginTop: '100px' }}>
         {/* Added Firestore UI */}
         <div style={{ marginTop: "20px" }}>
           <button onClick={saveUser}>Save My Info</button>
@@ -243,7 +162,6 @@ useEffect(() => {
             </div>
           )}
         </div>
-
       </div>
     </div>
   );
@@ -252,11 +170,20 @@ useEffect(() => {
 /* AUTH WRAPPER */
 export default function App() {
   const [user, setUser] = useState(null);
+  const [showGame, setShowGame] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        if (!currentUser.email.endsWith("@lewisu.edu")) {
+          alert("Please use your school email.");
+          signOut(auth);
+          return;
+        }
+      }
       setUser(currentUser);
     });
+    
     return () => unsubscribe();
   }, []);
 
@@ -275,26 +202,14 @@ export default function App() {
     );
   }
 
-  return <Game user={user} />;
+  return (
+    <div>
+      <button onClick={() => setShowGame(false)} style={{ marginTop: "80px" }}>Profile</button>
+      <button onClick={() => setShowGame(true)}>Game</button>
+      {showGame ? <Game user={user} /> : <Profile user={user} />}
+    </div>
+  );
 }
 
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+
